751f58a0657a2aa7399c39e1334fc642f079b90d
srushti kulkarni <srushtikulkarni15@gmail.com>
2024-06-14 19:49:43 +0530

    Done with section 18

--- Section 18/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Section 18/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,9 +1,5 @@
-var arr = [2, 3, 5, 6, 7, 8, 11, 10, 43];
-for (var i = 0; arr.length > i; i++) {
-  console.log(arr[i]);
-  if (arr[i] % 2 === 0) {
-    console.log(arr[i] + "  is Even number");
-  } else {
-    console.log(arr[i] + " is odd number");
-  }
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked");
+  });
 }
