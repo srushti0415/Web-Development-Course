dc4f1b27d7967ede939056a7089f01eb41eab8e6
srushti kulkarni <srushtikulkarni15@gmail.com>
2024-06-23 20:52:57 +0530

    Done with section 24

---
+++ Section 24/3.2 HTTP Requests/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Sunshine /</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<p>Hello moonshine/</p>");
+});
+
+app.listen(port, () => {
+  console.log(`server started on poart ${port}`);
+});
