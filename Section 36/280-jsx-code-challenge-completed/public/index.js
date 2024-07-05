a00c036206caebb0cd3ac8da825f95d4bd6206aa
srushti kulkarni <srushtikulkarni15@gmail.com>
2024-07-05 19:40:07 +0530

    Done with section 36

---
+++ Section 36/280-jsx-code-challenge-completed/public/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>My top 3 fav books all the time</h1>
+    <ul>
+      <li>Mruntanjay</li>
+      <li>Shiva Triology </li>
+      <li>Sherlock Holmes</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
