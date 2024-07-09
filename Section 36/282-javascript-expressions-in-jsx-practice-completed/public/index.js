0bb3c307d7479d41211f924aab41860a2646c64a
srushti kulkarni <srushtikulkarni15@gmail.com>
2024-07-09 18:23:30 +0530

    Done with section 36

--- Section 36/282-javascript-expressions-in-jsx-practice-completed/public/index.js
+++ Section 36/282-javascript-expressions-in-jsx-practice-completed/public/index.js
@@ -1,11 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-const name = "Srushti";
-ReactDOM.render(
-  <div>
-    <p>Created by {name}</p>
-    <p> CopyRight {" " + new Date().getFullYear()}</p>
-  </div>,
-  document.getElementById("root")
-);
+// const name = "Srushti";
+// ReactDOM.render(
+//   <div>
+//     <p>Created by {name}</p>
+//     <p> CopyRight {" " + new Date().getFullYear()}</p>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const img = "https://picsum.photos/200";
+
+// ReactDOM.render(
+//   <div>
+//     <h1>My Favourite Foods</h1>
+//     <img src="{img}" />
+//   </div>,
+//   document.getElementById("root")
+// );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// const customStyle = {
+//   color : "green",
+//   border: "3px solid red",
+//   fontsize: "20px"
+// };
+
+// customStyle.color : "pink";
+
+// ReactDOM.render(
+//     <h1 style= {customStyle}>My Favourite Foods</h1>,
+//   document.getElementById("root")
+// );
